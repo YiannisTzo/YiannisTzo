554f4e7bd30d353241b431a99e3bb0a9908c234a
Ioannis-Tzomiadis-NTT <ioannis.tzomiadis@nttdata.com>
2023-12-15 16:40:14 +0000

    added provider.tf

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+  /*
+    backend "s3" {
+    bucket = "mybucket"
+    key    = "path/to/my/key"
+    region = "us-east-1"
+  }
+  */
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
