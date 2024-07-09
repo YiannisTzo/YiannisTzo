acca19258862fa4c697f20ad1a85dd1298fb84ff
Ioannis Tzomiadis <itzomiad@emeal.nttdata.com>
2024-07-09 19:32:28 +0100

    edit compute.tf

--- Compute/compute.tf
+++ Compute/compute.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2_instance" {
-  ami           = "ami-08a52ddb321b32a8c"
-  instance_type = "us-east-2"
+  ami           = "ami-06c68f701d8090592"
+  instance_type = "t2.micro"
 
   tags = {
     default = "my first vm"
